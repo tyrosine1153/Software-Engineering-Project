05c825c907d0d9767c78f25db1c45eb9f9d4c51f
Yu Jeongmin <tyrosine1153@gmail.com>
2022-04-16 14:22:14 +0900

    Edit Video settings - to save settings

--- SE-Project/Assets/Scripts/VideoManager.cs
+++ SE-Project/Assets/Scripts/VideoManager.cs
@@ -5,17 +5,25 @@ public class VideoManager : Singleton<VideoManager>
     public Resolution[] Resolutions;
     public int currentResolutionIndex;
     public bool currentFullscreen;
+    
+    private const string ResolutionIndexKey = "RESOLUTION_INDEX";
+    private const string FullscreenKey = "FULLSCREEN";
+    
     protected override void Awake()
     {
         base.Awake();
         
         Resolutions = Screen.resolutions;
+        currentResolutionIndex = PlayerPrefs.GetInt(ResolutionIndexKey, currentResolutionIndex);
+        currentFullscreen = PlayerPrefs.GetInt(FullscreenKey, currentFullscreen ? 1 : 0) == 1;
     }
     public void SetResolution(int index, bool fullscreen)
     {
         if (index < 0 || index >= Resolutions.Length) return;
         currentResolutionIndex = index;
         currentFullscreen = fullscreen;
+        PlayerPrefs.SetInt(ResolutionIndexKey, currentResolutionIndex);
+        PlayerPrefs.SetInt(FullscreenKey, currentFullscreen ? 1 : 0);
         Screen.SetResolution(Resolutions[index].width, Resolutions[index].height, fullscreen);
     }
 } 
