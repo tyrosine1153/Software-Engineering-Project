ac1de32dcb7e748df8296884709d129c274e4964
Yu Jeongmin <tyrosine1153@gmail.com>
2022-04-28 15:45:10 +0900

    Fix data food to potion

---
+++ SE-Project/Assets/Scripts/Data/PotionModel.cs
@@ -0,0 +1,15 @@
+﻿public struct Potion  // json
+{
+    public int ID;
+    public string Name;
+    public string Description;
+    public Material PotionType;
+    public int[] Material;
+}
+
+public enum Material
+{
+    R,
+    G,
+    B,
+}
