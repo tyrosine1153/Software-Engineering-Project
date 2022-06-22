adaa28f242a4e69e57a3d82c44012fb07a81b3a7
Ws-Peroth <a01033056809@gmail.com>
2022-06-22 20:35:23 +0900

    Add Story No.42

--- SE-Project/Assets/Scripts/Data/OrderModel.cs
+++ SE-Project/Assets/Scripts/Data/OrderModel.cs
@@ -7,7 +7,7 @@ public class OrderOption
     // -1의 경우 정의된 옵션 외의 값을 뜻함
     public int potionId;
     public int nextScenarioID;
-    public int result;  // 결과값 (임시) 
+    public int result;  // 결과값 (임시) => 엔딩 분기를 결정하는 분기 포인트? : 아직 미구현 기능이기에 result
     
     public OrderOption(int potionId, int nextScenarioID, int result)
     {
@@ -22,5 +22,5 @@ public class EndingPoint
 {
     public int id;
     public int nextScenarioID;
-    public int result;  // 결과값 (임시)
+    public int result;  // 결과값 (임시) => 엔딩 분기를 결정하는 분기 포인트? : 아직 엔딩 결정이 미구현 임으로 result로 해둠
 }
